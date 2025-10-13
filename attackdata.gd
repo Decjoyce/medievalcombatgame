8243ac158f2922685c215e91dbac1fb75f7bac69
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2025-10-13 16:47:23 +0100

    john

---
+++ attackdata.gd
@@ -0,0 +1,9 @@
+class_name AttackData
+extends Resource
+
+@export var hand_attack_strings : Array[int]
+@export var hand_attacking : Array[int]
+
+@export var recovery_time: float
+@export var wind_up_time: float
+@export var attack_time: float
--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -25,6 +25,7 @@ func _process(delta: float) -> void:
 func enemy_entered(_new_enemy: Entity) -> void:
 	stance_graphic.visible = true
 	enemy = _new_enemy as Player
+	first_attack()
 	if enemy:
 		look_at(enemy.position)
 	else:
@@ -47,3 +48,74 @@ func _on_timer_2_timeout() -> void:
 	stance.attack_opponent(rng.randi_range(0, 1))
 	timer2.wait_time = rng.randf_range(0.5, 3.0)
 	timer2.start()
+
+@onready var timer_rec: Timer = $Timer_Recovery
+@onready var timer_windup: Timer = $Timer_Windup
+@onready var timer_attack: Timer = $Timer_Attack
+
+@onready var is_attacking: bool = false
+
+func first_attack() -> void:
+	timer_rec.wait_time = attack_queue[current_attack_index].recovery_time
+	timer_windup.wait_time = attack_queue[current_attack_index].wind_up_time
+	timer_attack.wait_time = attack_queue[current_attack_index].attack_time
+	
+	is_attacking = false
+	timer_rec.start()
+
+func new_attack() -> void:
+	current_attack_index = + 1
+	if current_attack_index >= attack_queue.size():
+		current_attack_index = 0
+	
+	timer_rec.wait_time = attack_queue[current_attack_index].recovery_time
+	timer_windup.wait_time = attack_queue[current_attack_index].wind_up_time
+	timer_attack.wait_time = attack_queue[current_attack_index].attack_time
+	
+	is_attacking = false
+	timer_rec.start()
+
+func recover() -> void:
+	pass
+
+func windup() -> void:
+	is_attacking = true
+	for i in attack_queue[current_attack_index].hand_attack_strings.size():
+		stance.occupy_slot(attack_queue[current_attack_index].hand_attack_strings[i], i)
+	
+
+@export var attack_queue: Array[AttackData]
+@export var current_attack_index: int
+
+func attack_q() -> void:
+	for i in attack_queue[current_attack_index].hand_attacking.size():
+		stance.attack_opponent(i)
+	#sprite changing thing
+
+@export var uiattack : Array[Sprite3D]
+
+#Or Windup begin
+func _on_recovery_end() -> void:
+	print("John")
+	windup()
+	for i in attack_queue[current_attack_index].hand_attack_strings.size():
+		var dog = uiattack[attack_queue[current_attack_index].hand_attack_strings[i]]
+		var tween = get_tree().create_tween()
+		tween.tween_property(dog, "scale", Vector2(), 1.0).set_trans(Tween.TRANS_BOUNCE)
+	timer_windup.start()
+
+#Attack Begin
+func _on_wind_up_end() -> void:
+	print("Cena")
+	attack_q()
+	timer_attack.start()
+
+#Recovery Begin
+func _on_attack_end() -> void:
+	print("69")
+	recover()
+	new_attack()
+
+
+func _on_timer_recovery_timeout() -> void:
+	pass # Replace with function body.
